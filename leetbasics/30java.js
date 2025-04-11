ed78f8059afe3ade391212b6f8a2f52c9b9e288b
amaanansari2604 <amaanansari790786@gmail.com>
2025-04-11 17:46:24 +0530

    igmt 2

--- leetbasics/30java.js
+++ leetbasics/30java.js
@@ -28,3 +28,37 @@ console.log(f(1,2,3))
 console.log(f("anything"))
 console.log(f({},null, 42))
 
+// 2620. Counter
+// Solved
+// Easy
+// Companies
+// Hint
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+ 
+
+// Example 1:
+
+// Input: 
+// n = 10 
+// ["call","call","call"]
+// Output: [10,11,12]
+// Explanation: 
+// counter() = 10 // The first time counter() is called, it returns n.
+// counter() = 11 // Returns 1 more than the previous time.
+// counter() = 12 // Returns 1 more than the previous time.
+
+function createCounter(n){
+    let counter = n;
+    return function(){
+        return counter++
+    }
+}
+
+const counter = createCounter(-2)
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+
