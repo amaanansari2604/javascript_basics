9f3d2ebbb32365626ddcc3d460616db711b0ae44
amaanansari2604 <amaanansari790786@gmail.com>
2025-04-11 18:38:13 +0530

    igmt-6

--- leetbasics/30java.js
+++ leetbasics/30java.js
@@ -194,5 +194,32 @@ const arr = [1,2,3]
 const newArray = map(arr, plusone)
 
 
+// 2634. Filter Elements from Array
+// Easy
+// Companies
+// Hint
+// Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
 
+// The fn function takes one or two arguments:
 
+// arr[i] - number from the arr
+// i - index of arr[i]
+// filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+
+// Please solve it without the built-in Array.filter method.
+
+function filter(arr, fn){
+    let result = []
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i],i)){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+function greaterthan(n){
+    return n > 10;
+}
+const newarr = [0,10,20,30]
+const arraynew = filter(arr, greaterthan)
